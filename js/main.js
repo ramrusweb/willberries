352eae044ec33bfd59664fdb3ed4a5ef829a8c8e
rrm1 <rustamramazanovworkmail@gmail.com>
2021-04-02 23:10:45 +0300

    (add)smooth scrolling to the top

--- js/main.js
+++ js/main.js
@@ -30,3 +30,20 @@ modalCart.addEventListener('click', event => {
 		closeModal()
 	}
 })
+
+// scroll smooth
+
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link')
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', event => {
+			event.preventDefault()
+			const id = scrollLinks[i].getAttribute('href')
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			})
+		})
+	}
+}
